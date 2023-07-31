83a6271e7901eecc5d871efce3166e7854b15e68
abi-hong <flffjakfwm20@gmail.com>
2023-07-31 15:20:07 +0900

    feat: textarea 이벤트 완성중

--- src/components/ChatInputBox.js
+++ src/components/ChatInputBox.js
@@ -10,36 +10,42 @@ export default function ChatInputBox() {
     const submitMessage = (e) => {
         //console.log(e);
         if (e.nativeEvent.isComposing) { // isComposing 이 true 이면
-            textArea.current.style.height = 'auto';
-            textArea.current.style.height = textArea.current.scrollHeight + 'px';
             return;// 조합 중이므로 동작을 막는다.
         }
 
         if (e.key === 'Enter' && e.shiftKey) { // [shift] + [Enter] 치면 걍 리턴
+            //textArea.current.style.height = 'auto';
+            //textArea.current.style.height = textArea.current.scrollHeight + 'px';
+            //console.log('textArea.current.scrollHeight', textArea.current.scrollHeight);
             return;
-        } else if (e.key === 'Enter') { // [Enter] 치면 메시지 보내기
+        } 
+        else if (e.key === 'Enter') { // [Enter] 치면 메시지 보내기
             console.log(e.target.value);
-            e.target.value = "";
+            textArea.current.value = "";
+            textArea.current.blur();
+            textArea.current.style.height = 'auto';
             //sendText(e);
         }
     };
 
-    const handleResizeHeight = (e) => {
-        console.log(e);
-        textArea.current.style.height = 20+'px';
-        textArea.current.style.height = textArea.current.scrollHeight + 'px';
-        //console.log('before btn.current.style.height', btn.current.style.height);
-        //btn.current.style.height = btn.current.style.height + textArea.current.scrollHeight + 'px';
-        //console.log('after btn.current.style.height', btn.current.style.height);
+    const resizeScrollHeight = (e) => {
+        let row = e.target.value.split('\n').length;
+        if () {
+            console.log('row', row);
+            textArea.current.style.height = 'auto';
+            textArea.current.style.height = textArea.current.scrollHeight + 'px';
+            console.log('textArea.current.scrollHeight', textArea.current.scrollHeight);
+        }
     }
 
     return (
         <div className='chat-input-box'>
             <button className='chat-input-box-btn' ref={btn}></button>
             <textarea rows={1} className='chat-input-box-textarea'
-                onKeyDown={submitMessage}
-                /*onChange={handleResizeHeight}*/
-                ref={textArea} placeholder='궁금한 내용을 입력해주세요.'></textarea>
+                onKeyDown={submitMessage} 
+                onChange={resizeScrollHeight} 
+                ref={textArea} placeholder='궁금한 내용을 입력해주세요.'>
+            </textarea>
         </div>
     );
 }

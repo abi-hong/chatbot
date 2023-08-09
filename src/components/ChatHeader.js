27e5b7da326e29eb6723677b71d49185320a5c23
abi-hong <flffjakfwm20@gmail.com>
2023-08-09 13:48:55 +0900

    fix: 챗봇 헤더 CSS 수정

--- src/components/ChatHeader.js
+++ src/components/ChatHeader.js
@@ -4,9 +4,7 @@ export default function ChatHeader() {
     return (
         <div className='chat-header'>
             <div className='chat-header-title'>Chatbot</div>
-            <div className='chat-header-btn'>
-                <input type="button" value="상담종료" />
-            </div>
+            <button className='chat-header-btn'>상담종료</button>
         </div>
     );
 }

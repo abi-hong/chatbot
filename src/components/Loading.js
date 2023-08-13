437591f90fa1178a372a1a69fa208cf99bcc595d
abi-hong <flffjakfwm20@gmail.com>
2023-08-13 16:10:56 +0900

    feat: Loading Component 만들기

---
+++ src/components/Loading.js
@@ -0,0 +1,10 @@
+export default function Loading() {
+    return (
+        <div className="answer-box">
+            <div className="time-div">
+                <span className="time-span">{/*{props.time}*/}</span>
+            </div>
+            <p className="answer-text">Loading...</p>
+        </div>
+    );
+}

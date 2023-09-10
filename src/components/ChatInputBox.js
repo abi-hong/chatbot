d92f20c19356ee2189323f871502783146d68fb5
abi-hong <flffjakfwm20@gmail.com>
2023-09-10 16:05:07 +0900

    [fix] 클릭시 이미지 바뀜 수정

--- src/components/ChatInputBox.js
+++ src/components/ChatInputBox.js
@@ -5,10 +5,10 @@ import { question } from '../store/slice';
 import { asyncAnswerFetch } from '../store/slice';
 import CloseImg from '../constants/close-quick-menu.png';
 import Img from '../constants/quick-menu.png';
+import QuickMenu from './QuickMenu';
 
 // 엔터 누르면 전송되도록
 export default function ChatInputBox() {
-    const [imgQuickMenu, setImgQuickMenu] = useState(Img);
     const [isClicked, setIsClicked] = useState(false);
 
     const dispatch = useDispatch();
@@ -36,34 +36,37 @@ export default function ChatInputBox() {
         }
     };
 
-    return (
-        <div className='chat-input-box'>
-            <button className='chat-input-button' 
-                onClick={(e) => {
-                    //console.log('click!');
-                    if (isClicked) {
-                        setImgQuickMenu(CloseImg);
-                        setIsClicked(false);
-                    } else {
-                        setImgQuickMenu(Img);
-                        setIsClicked(true);
-                    }
-                }}
-            ><img src={imgQuickMenu} width={18} height={15}/></button>
-            <textarea rows={1} className='chat-input-box-textarea'
-                onKeyDown={(e) => {
-                    if (e.key === 'Enter') {
-                        console.log('e.target.value', e.target.value);
-                        dispatch(question(e.target.value));
-                        dispatch(asyncAnswerFetch(e.target.value));
-                    }
-                }
-                }
-                /*onChange={resizeScrollHeight}*/
-                ref={textArea} placeholder='궁금한 내용을 입력해주세요.'>
-            </textarea>
-            <button className='chat-input-box-btn'>전송</button>
-        </div>
+    const showQuickMenu = (e) => {
+        if (isClicked) {
+            setIsClicked(false);
+        } else {
+            setIsClicked(true);
+        }
+    };
 
+    return (
+        <>
+            <div className='chat-input-box'>
+                <button className='chat-input-button'
+                    onClick={showQuickMenu}>
+                    {isClicked ? <img src={CloseImg} width={18} height={15} /> 
+                        : <img src={Img} width={18} height={15} /> }
+                </button>
+                <textarea rows={1} className='chat-input-box-textarea'
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                            console.log('e.target.value', e.target.value);
+                            dispatch(question(e.target.value));
+                            dispatch(asyncAnswerFetch(e.target.value));
+                        }
+                    }
+                    }
+                    /*onChange={resizeScrollHeight}*/
+                    ref={textArea} placeholder='궁금한 내용을 입력해주세요.'>
+                </textarea>
+                <button className='chat-input-box-btn'>전송</button>
+            </div>
+            <QuickMenu className={isClicked ? 'show-menu' : 'hide-menu'}></QuickMenu>
+        </>
     );
 }
